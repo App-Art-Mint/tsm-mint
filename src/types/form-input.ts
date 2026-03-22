aa1fb8e47ada274d6d3eaae4e15a0f5259779c9f
Sam Underwood <sam@appartmint.com>
2026-03-22 14:11:11 -0400

    v0.1.15 - form-input types

---
+++ src/types/form-input.ts
@@ -0,0 +1,5 @@
+export type TextTypes = 'text' | 'textarea' | 'search' | 'tel' | 'url' | 'email' | 'password';
+export type NumberTypes = 'number' | 'month' | 'week';
+export type BooleanTypes = 'radio' | 'checkbox';
+
+export type InputType = BooleanTypes | TextTypes | NumberTypes | 'color' | 'hidden' | 'image';
